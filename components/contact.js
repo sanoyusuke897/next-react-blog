a38e3f9d3b7f04ef300c4cd458cc2c65127fe208
sanoyusuke897 <sano@alivenet.com>
2023-02-04 17:39:27 +0900

    メールアドレス修正

--- components/contact.js
+++ components/contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
         <div className={styles.stack}>
             <h3 className={styles.heading}>Contact</h3>
             <Social iconSize="30px" />
-            <address>info#sanoyusuke.com</address>
+            <address>yuusukure#gmail.com</address>
         </div>
     )
 }
