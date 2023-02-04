6f2feac9274d98d859e77b107eabb6c637840c83
sanoyusuke897 <sano@alivenet.com>
2023-02-04 15:59:17 +0900

    noreffer修正

--- components/social.js
+++ components/social.js
@@ -7,19 +7,19 @@ export default function Social({ iconSize = 'initial' }) {
     return (
         <ul className={styles.list} style={{ '--icon-size': iconSize }}>
             <li>
-                <a target="_blank" href="https://twitter.com/Tippy_uscpa">
+                <a target="_blank" href="https://twitter.com/Tippy_uscpa" rel="noreferrer">
                     <FontAwesomeIcon icon={faTwitter} />
                     <span className="sr-only">Twitter</span>
                 </a>
             </li>
             <li>
-                <a target="_blank" href="https://www.facebook.com">
+                <a target="_blank" href="https://www.facebook.com" rel="noreferrer">
                     <FontAwesomeIcon icon={faFacebookF} />
                     <span className="sr-only">Facebook</span>
                 </a>
             </li>
             <li>
-                <a target="_blank" href="https://github.com/sanoyusuke897">
+                <a target="_blank" href="https://github.com/sanoyusuke897" rel="noreferrer">
                     <FontAwesomeIcon icon={faGithub} />
                     <span className="sr-only">GitHub</span>
                 </a>
