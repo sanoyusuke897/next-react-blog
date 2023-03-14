e854f31c0d6627d4d54a990de4e843dd18c2bff9
Yusuke Sano <98931362+sanoyusuke897@users.noreply.github.com>
2023-03-14 16:52:02 +0900

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -1,6 +1,6 @@
 import styles from 'styles/hero.module.css'
 import Image from 'next/image'
-import cube from 'images/top.jpeg'
+import cube from 'images/top.png'
 
 export default function Hero({ title, subtitle, imageOn = false}) {
     return (
